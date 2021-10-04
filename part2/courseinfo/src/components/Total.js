0647286ac3379ff6c24f341a07039360c333cf26
rommelniebres <rommelniebresj@gmail.com>
2021-10-04 19:03:20 +0800

    Done part2.3

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 const Total = ({ parts }) => {
-    let sum = 0
-
-    parts.map(part => 
-      sum += part.exercises
-    )
-    return(
-      <p style={{fontWeight: 'bold'}}>Number of exercises {sum}</p>
-    ) 
+  // const total = parts.reduce(function(previousPart, currentPart) {
+  //   console.log(previousPart.exercises, currentPart.exercises);
+  // }, 0);
+  
+  const total = parts.reduce((previousValue, currentValue) => {
+    return {exercises: previousValue.exercises + currentValue.exercises} 
+  })
+    
+  return(
+    <p style={{fontWeight: 'bold'}}>Number of exercises {total.exercises}</p>
+  ) 
   }
   
 export default Total
